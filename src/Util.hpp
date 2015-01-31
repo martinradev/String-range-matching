1ca105f1a3251b51247509fb7de7bc539405dea1
martinradev <martin.b.radev@gmail.com>
2015-01-31 16:26:19 +0200

    added more documentation to string range algo

--- src/Util.hpp
+++ src/Util.hpp
@@ -6,6 +6,11 @@
 #include <boost/dynamic_bitset.hpp>
 
 namespace str {
+    /*!
+        given two bit vectors \a lowBits and \a topBits it finds the
+        bits for which we have ones only in either one of them
+        It returns the positions of those bits.
+    */
     std::vector<size_t> retrieveRangeIndices(const boost::dynamic_bitset<> & lowbits, const boost::dynamic_bitset<> & topbits);
 };
 
