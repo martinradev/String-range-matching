adc2befe6379e86199ed192801518aad282f82a7
Jarno Leppänen <jarno.leppanen@iki.fi>
2015-02-02 21:30:37 +0200

    Add features to rmatch

--- src/naive_string_range_reporting.hpp
+++ src/naive_string_range_reporting.hpp
@@ -2,20 +2,40 @@
 #define NAIVE_STRING_RANGE_REPORTING_HPP
 
 #include <algorithm>
+#include <string>
+#include <iterator>
 
 namespace str {
 
 template <typename input_iterator, typename output_iterator>
 void naive_range_match(
-        input_iterator xi, input_iterator xe
-        input_iterator yi, input_iterator ye,
+        input_iterator ti, input_iterator te,
+        input_iterator bi, input_iterator be,
+        input_iterator ei, input_iterator ee,
         output_iterator r)
 {
     using namespace std;
     typename iterator_traits<output_iterator>::value_type i = 0;
-    for (size_t i = 0; xi != xe; ++i, ++r) {
-        if (lexicographic_compare(xi,xe,yi,ye)) *r = i;
+    for (; ti != te; ++ti, ++i, ++r) {
+        if (
+                 lexicographical_compare(ti,te,ei,ee) &&
+                !lexicographical_compare(ti,te,bi,be)) *r = i;
     }
 }
 
+template <typename char_type, typename output_iterator>
+void naive_range_match(
+        const std::basic_string<char_type>& t,
+        const std::basic_string<char_type>& b,
+        const std::basic_string<char_type>& e,
+        output_iterator r)
+{
+    naive_range_match(
+            t.begin(), t.end(),
+            b.begin(), b.end(),
+            e.begin(), e.end(), r);
+}
+
 } // str
+
+#endif
--- src/rmatch.cpp
+++ src/rmatch.cpp
@@ -1,73 +1,275 @@
 //#include "Crochemore.hpp"
 #include "one_sided_string_range_counting.hpp"
 #include "one_sided_string_range_counting_ostream.hpp"
+#include "naive_string_range_reporting.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
 #include <cstdlib>
-#include <unistd.h>
+#include <cstring>
+#include <cstdarg>
+#include <getopt.h>
 
-/*
-    implement crochemore
-    comment crochemore
-    write tests
-    implement prefix query using crochemore idea
-    write good tests
-    write tester
-    check
-    implement prefix query using stored border information using z algorithm and ones using kmp
-    test
-    document everything
-    create poster
-*/
+using namespace std;
 
-void usage(FILE *f, char *argv[])
+enum method {
+    NAIVE,
+    COUNT
+};
+
+struct input {
+    string t;
+    string b;
+    string e;
+    method m;
+    int k;
+    int s;
+    int ret;
+    input(): k(3), m(NAIVE), s(false), ret(0) {}
+};
+
+const char *shopts = "hm:k:sf:t:";
+
+const option opts[] = {
+    { "help",   no_argument,       nullptr, 'h' },
+    { "method", required_argument, nullptr, 'm' },
+    { "k",      required_argument, nullptr, 'k' },
+    { "s",      no_argument,       nullptr, 's' },
+    { nullptr,  no_argument,       nullptr,  0  }
+};
+
+const char *help_str = R"STR(
+Mandatory arguments to long options are mandatory for short options too.
+  -h, --help           display this help and exit
+  -m, --method=METHOD  set the matching algorithm; possible values are "naive",
+                         or "count", default is "naive"
+  -k, --k=VALUE        run range match count k set to VALUE, only has effect if
+                         METHOD is "count"; k must be larger or equal to 3,
+                         default is 3
+  -s, --silent         do not produce any output
+  -f, --file=FILE      load text from file FILE
+  -t, --test=TESTFILE  load test file from file TESTFILE
+)STR";
+
+void usage(FILE *f, const char *app)
 {
-    fprintf(f, "Usage: %s [OPTION] TEXT PATTERN\n", argv[0]);
+    fprintf(f, "Usage: %s [OPTION] TEXT BEGIN END     (1st form)\n", app);
+    fprintf(f, " or:   %s [OPTION] -f FILE BEGIN END  (2nd form)\n", app);
+    fprintf(f, " or:   %s [OPTION] -t TESTFILE        (3rd form)\n", app);
 }
 
-void help(FILE *f, char *argv[])
+void help(FILE *f, const char *app)
 {
-    usage(stdout, argv);
-    printf("  -h         display this help and exit\n");
-    printf("  -k VALUE   run range match count with k set to VALUE;\n");
-    printf("             k must be larger or equal to 3, default is 3\n");
+    usage(f,app);
+    fprintf(f,help_str);
 }
 
-int main(int argc, char *argv[])
+void nag(const char *app, const char *fmt...)
 {
-    using namespace str;
+    va_list args;
+    va_start(args, fmt);
+    fprintf(stderr, "%s: ", app);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+bool readfile(const char *file, string& s)
+{
+    ifstream t(file);
+    if (!t.good()) return false;
+    t.seekg(0, ios::end);
+    size_t size = t.tellg();
+    s.resize(size);
+    t.seekg(0);
+    t.read(&s[0], size);
+    return true;
+}
+
+bool readtestfile(const char *file, input& in)
+{
+    ifstream t(file);
+    if (!t.good()) return false;
+    getline(t,in.t);
+    if (!t.good()) return false;
+    getline(t,in.b);
+    if (!t.good()) return false;
+    getline(t,in.e);
+    // ignore suffixes
+    return true;
+}
+
+bool fail(input& in)
+{
+    in.ret = 1;
+    return false;
+}
+
+extern "C" bool init(int argc, char *const argv[], input& in)
+{
     char c;
-    int k = 3;
-    while ((c = getopt(argc, argv, "hk:")) != -1) {
+    const char *app = argv[0];
+    int form = 1;
+    while ((c = getopt_long(argc, argv, shopts, opts, nullptr)) != -1) {
         switch (c) {
             case 'h':
-                help(stdout, argv);
-                return 0;
+                help(stdout, app);
+                return false;
+            case 'm':
+                if (!strcmp(optarg,"naive")) {
+                    in.m = NAIVE;
+                } else if (!strcmp(optarg,"count")) {
+                    in.m = COUNT;
+                } else {
+                    nag(app,"uknown method \"%s\"\n",optarg);
+                    return fail(in);
+                }
+                break;
             case 'k':
-                k = atoi(optarg);
-                if (k < 3) {
-                    fprintf(stderr, "k must be larger or equal to 3\n");
-                    return 1;
+                in.k = atoi(optarg);
+                if (in.k < 3) {
+                    nag(app,"k must be an integer larger or equal to 3\n");
+                    return fail(in);
+                }
+                break;
+            case 's':
+                in.s = true;
+                break;
+            case 'f':
+                form = 2;
+                if (!readfile(optarg,in.t)) {
+                    nag(app,"can't read file %s\n",optarg);
+                    return fail(in);
+                }
+                break;
+            case 't':
+                form = 3;
+                if (!readtestfile(optarg,in)) {
+                    nag(app,"can't read test file %s\n",optarg);
+                    return fail(in);
                 }
                 break;
             case '?':
-                if (optopt == 'k') {
-                    fprintf(stderr, "Option -%c  requires an integer value.\n");
-                } else if (isprint(optopt)) {
-                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-                } else {
-                    fprintf(stderr, "Unknown option character '0x%x'.\n",
-                            optopt);
-                }
-                return 1;
             default:
-                return 1;
+                // getopt prints errors
+                return fail(in);
         }
     }
-    if (optind+2 > argc) {
-        help(stderr, argv);
-        return 1;
+    switch (form) {
+        case 1:
+            if (optind+3 > argc) {
+                //nag(app,"expected TEXT and BEGIN and END patterns\n");
+                help(stderr,app);
+                return fail(in);
+            }
+            in.t = argv[optind];
+            in.b = argv[optind+1];
+            in.e = argv[optind+2];
+            break;
+        case 2:
+            if (optind+1 > argc) {
+                //nag(app,"expected BEGIN and END patterns\n");
+                help(stderr,app);
+                return fail(in);
+            }
+            in.b = argv[optind];
+            in.e = argv[optind+1];
+            break;
+        case 3:
+        default:
+            break;
     }
-    const char *text = argv[optind];
-    const char *pattern = argv[optind+1];
-    printf("%d\n", one_sided_string_range_counting(text,pattern,k));
+    return true;
+}
+
+// the following stuff enables us to exclude output vector allocations
+// from memory profiling
+
+struct output {
+    vector<size_t> s;
+};
+
+// this can be ignored in valgrind for accurate memory profiling
+extern "C" void push_back(output *out, size_t x)
+{
+    out->s.push_back(x);
+}
+
+struct push_back_it {
+    output *o;
+    push_back_it(output& o): o(&o) {}
+    size_t& operator=(size_t x) { push_back(o,x); return o->s.back(); }
+    push_back_it& operator*() { return *this; }
+    push_back_it& operator++() { return *this; }
+};
+
+namespace std {
+
+template <>
+struct iterator_traits<push_back_it> {
+    typedef size_t value_type;
+};
+
+} // std
+
+struct print_it {
+    size_t n;
+    size_t& operator=(size_t x) { cout << x << '\n'; return n; }
+    print_it& operator*() { return *this; }
+    print_it& operator++() { return *this; }
+};
+
+namespace std {
+
+template <>
+struct iterator_traits<print_it> {
+    typedef size_t value_type;
+};
+
+} // std
+
+// no-operation output iterator, use this for accurate memory profiling
+
+struct nop_it {
+    int n;
+    int& operator=(int) { return n; }
+    nop_it& operator*() { return *this; }
+    nop_it& operator++() { return *this; }
+};
+
+namespace std {
+
+template <>
+struct iterator_traits<nop_it> {
+    typedef int value_type;
+};
+
+} // std
+
+void run_naive(const input& in)
+{
+    str::naive_range_match(in.t,in.b,in.e,print_it());
+}
+
+void run_naive_silent(const input& in)
+{
+    str::naive_range_match(in.t,in.b,in.e,nop_it());
+}
+
+int main(int argc, char *const argv[])
+{
+    input in;
+    size_t c;
+    if (!init(argc, argv, in)) return in.ret;
+    switch (in.m) {
+        case NAIVE:
+            if (in.s) run_naive_silent(in);
+            else run_naive(in);
+            break;
+        case COUNT:
+            c = str::one_sided_string_range_counting(in.t,in.e,in.k)-
+                str::one_sided_string_range_counting(in.t,in.b,in.k);
+            if (!in.s) printf("%d\n",c);
+            break;
+    }
     return 0;
 }
